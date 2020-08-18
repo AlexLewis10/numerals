d32acd781a3fe937048aeb39ca41f380b454c251
Alex Lewis <alexlewis374@gmail.com>
2020-08-18 15:44:56 +0100

    Refactor first if condition

--- src/numerals.js
+++ src/numerals.js
@@ -3,7 +3,7 @@ export default class Numerals {
   convert (number) {
     let numeralsArray = []
 
-    if (number % 5 === 0) {
+    if (number === 5) {
       numeralsArray.push("V")
     } else if (number < 5 && number < (5 - 1)) {
       for(let i = 0; i < number; i++) {
